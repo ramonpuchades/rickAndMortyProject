cb370c358b92d60390ac6f3f3f44ddc0709c81a1
ramonpuchades <110656030+ramonpuchades@users.noreply.github.com>
2022-11-14 17:14:08 +0100

    Add project template

---
+++ RickAndMortyAPIProject/RickAndMortyAPIProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPIProject
+//
+//  Created by User on 14/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
